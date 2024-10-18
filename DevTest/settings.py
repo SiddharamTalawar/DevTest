295307dad4103dd47c3fa2fc89cc0283508f2a7b
SiddharamTalawar <siddharamtalawar10@gmail.com>
2024-10-19 01:36:32 +0530

    added vercel allowed host

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d75@oicwp#7_)^!$^0i@s&vp-ln3pg()wpzfrro!@en5i!st&!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
